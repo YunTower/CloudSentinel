841a83420c31af8a872da6504a442747ee97bd50
ohhhfish <xyfishcn@qq.com>
2025-08-12 15:30:02 +0800

    ✨ 新增 Card 组件的颜色方案配置

--- src/main.ts
+++ src/main.ts
@@ -57,6 +57,30 @@ const Noir = definePreset(Aura, {
         }
       }
     }
+  },
+  components: {
+    card: {
+      colorScheme: {
+        light: {
+          root: {
+            background: '{surface.0}',
+            color: '{surface.700}'
+          },
+          subtitle: {
+            color: '{surface.500}'
+          }
+        },
+        dark: {
+          root: {
+            background: '{surface.900}',
+            color: '{surface.0}'
+          },
+          subtitle: {
+            color: '{surface.400}'
+          }
+        }
+      }
+    }
   }
 });
 
